3a243f686d91040a7e2a41b61918db26235ce46f
Evan Abbott <107959883+EvanA4@users.noreply.github.com>
2025-01-25 13:13:32 -0500

    small tweaks to initial chess board display

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,9 @@
     import '$lib/chessStyle.css';
 </script>
 
-<p>welcome to root</p>
+<p class="text-center text-[50px] text-white mt-10"><b>This is LaziBoard</b></p>
 
-<div class="w-[100vw] flex justify-center">
+<div class="w-[100vw] flex justify-center mt-10">
     <div class="w-[30vw]">
         <Chess class="cg-paper"/>
     </div>
